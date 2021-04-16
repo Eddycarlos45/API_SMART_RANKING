1f2ddc71990c47fb0ff95780708459988212d415
edsonsousa33 <edson.sousa@compasso.com.br>
2021-04-16 17:05:05 -0300

    create others features for players

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -5,14 +5,14 @@ import { PlayersService } from './players.service';
 
 const ackErrors: string[] = ['E11000']; //error cases where you can delete message in rabbitmq
 
-@Controller('api/v1')
+@Controller()
 export class PlayersController {
     constructor(private readonly playersService: PlayersService) {}
 
     logger = new Logger(PlayersController.name);
 
     @EventPattern('create-player')
-    async createCategory(
+    async createPlayer(
       @Payload() player: IPlayer,
       @Ctx() context: RmqContext,
     ) {
@@ -34,7 +34,7 @@ export class PlayersController {
     }
 
     @MessagePattern('get-players')
-    async getCategories(@Payload() _id: string, @Ctx() context: RmqContext) {
+    async getPlayer(@Payload() _id: string, @Ctx() context: RmqContext) {
       const channel = context.getChannelRef();
       const originalMessage = context.getMessage();
       try {
@@ -45,6 +45,45 @@ export class PlayersController {
         }
       } finally {
         await channel.ack(originalMessage);
+      }
+    }
+
+    @EventPattern('update-player')
+    async updatePlayer(@Payload() data: any, @Ctx() context: RmqContext) {
+      const channel = context.getChannelRef();
+      const originalMessage = context.getMessage();
+      this.logger.log(`data: ${JSON.stringify(data)}`);
+      try {
+        const _id: string = data.id;
+        const player: IPlayer = data.player;
+        await this.playersService.updatePlayer(_id, player);
+        await channel.ack(originalMessage);
+      } catch (error) {
+        const filterAckError = ackErrors.filter((ackError) =>
+          error.message.includes(ackError),
+        );
+        if (filterAckError) {
+          await channel.ack(originalMessage);
+        }
+      }
+    }
+
+    @EventPattern('delete-player')
+    async deletePlayer(@Payload() data: any, @Ctx() context: RmqContext) {
+      const channel = context.getChannelRef();
+      const originalMessage = context.getMessage();
+      this.logger.log(`data: ${JSON.stringify(data)}`);
+      try {
+        const _id: string = data._id;
+        await this.playersService.deletePlayer(_id);
+        await channel.ack(originalMessage);
+      } catch (error) {
+        const filterAckError = ackErrors.filter((ackError) =>
+          error.message.includes(ackError),
+        );
+        if (filterAckError) {
+          await channel.ack(originalMessage);
+        }
       }
     }
 }
